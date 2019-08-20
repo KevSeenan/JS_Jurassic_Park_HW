3bf518ab8bc3e601af9d7b2a61b6b3923ae0057a
KevSeenan <kev.seenan@gmail.com>
2019-08-20 16:58:24 +0100

    Adds test for collection of dinosaurs

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,9 +4,13 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
   let park;
+  let dinosaur1;
+  let dinosaur2;
 
   beforeEach(function () {
     park = new Park('Jurassic Park', 70);
+    dinosaur1 = new Dinosaur('Velociraptor');
+    dinosaur2 = new Dinosaur('Brachiosaurus');
   })
 
   it('should have a name', function(){
@@ -19,16 +23,22 @@ describe('Park', function() {
     assert.strictEqual(actual, 70);
   });
 
-  it('should have a collection of dinosaurs');
+    describe('dinosaurs', function(){
+      it('should have a collection of dinosaurs', function(){
+        const actual = park.collectionOfDinosaurs();
+        assert.deepStrictEqual(actual, 0);
+      });
 
-  it('should be able to add a dinosaur to its collection');
+      it('should be able to add a dinosaur to its collection');
 
-  it('should be able to remove a dinosaur from its collection');
+      it('should be able to remove a dinosaur from its collection');
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+      it('should be able to find the dinosaur that attracts the most visitors');
 
-  it('should be able to find all dinosaurs of a particular species');
+      it('should be able to find all dinosaurs of a particular species');
 
-  it('should be able to remove all dinosaurs of a particular species');
+      it('should be able to remove all dinosaurs of a particular species');
+    });
+
 
 });
